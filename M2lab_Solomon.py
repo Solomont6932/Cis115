d7fd8a2fae9ad8bffd72ca3ceb9c127c14aac52c
Solomont6932 <55244507+Solomont6932@users.noreply.github.com>
2019-09-12 10:44:04 -0400

    spyder

---
+++ M2lab_Solomon.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+M2T1
+Solomon
+9/12/19
+"""
+
+# Convert M2T1 into python 
+
+#	// M2LAB
+#	// Taylyn Solomon
+#	// 9/12/19
+	
+#	// Declarations 
+#	Declare string name 
+#	Declare integer age
+age = input()
+name = input()
+
+#	// Ask for their name
+#	Display "what's your name?"
+print("what's your name?")
+# Input name 
+name = input()
+print("hello" , name) 
+#	Display "hello" , name
+
+#	// Ask for their age
+#	Display"how old are you?"
+print("how old are you?")
+#	Input age 
+age = input()
+#	// Tell them (by name) how old they will be 
+#	// on their next birthday
+#	Set age = age + 1 
+age = age + 1 
+age = int(input())
+#	Display "you will be" , age , "next year."
+print("you will be" , age , "next year.")
